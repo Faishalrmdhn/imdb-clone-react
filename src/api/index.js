0ec001f0c7dc86dd023ef671ff4c898039cf8441
Faishalrmdhn <faishalramadhan25@gmail.com>
2024-01-29 23:52:02 +0700

    adjust config for api

--- src/api/index.js
+++ src/api/index.js
@@ -2,18 +2,22 @@ import axios from 'axios'
 
 const ApiService = {
 
-    baseURL : process.env.BASE_URL || 'https://api.themoviedb.org/3/',
+    baseURL : process.env.REACT_APP_BASE_URL,
     config : {
         headers: {
             'Content-Type': 'application/json'
         },
         params: {
-            'api_key': process.env.API_KEY
+            'api_key': process.env.REACT_APP_API_KEY
         }
     },
 
     popularMovies(){
-        return axios.get(`${this.baseURL}discover/movie?include_adult=false&include_video=false&language=en-US&page=2&sort_by=popularity.desc`)
+        return axios.get(`${this.baseURL}discover/movie?include_adult=false&include_video=false&language=en-US&page=2&sort_by=popularity.desc`,{
+            params: {
+                ...this.config.params
+            }
+        })
     }
 }
 
